63d42c7d3ec4ddc0ae1983eed12836fe4ebefb97
nguyenthinga2703dinhthihuyen2592 <95742902+nguyenthinga2703dinhthihuyen2592@users.noreply.github.com>
2022-01-04 20:59:24 +0700

    Add files via upload

---
+++ src/userprog/build/userprog/syscall.d
@@ -0,0 +1,9 @@
+userprog/syscall.o: ../../userprog/syscall.c ../../userprog/syscall.h \
+ ../../threads/synch.h ../../lib/kernel/list.h ../../lib/stdbool.h \
+ ../../lib/stddef.h ../../lib/stdint.h ../../threads/thread.h \
+ ../../lib/debug.h ../../lib/stdio.h ../../lib/stdarg.h \
+ ../../lib/kernel/stdio.h ../../lib/syscall-nr.h \
+ ../../threads/interrupt.h ../../threads/malloc.h ../../threads/vaddr.h \
+ ../../threads/loader.h ../../userprog/pagedir.h ../../userprog/process.h \
+ ../../lib/user/syscall.h ../../devices/input.h ../../devices/shutdown.h \
+ ../../filesys/file.h ../../filesys/off_t.h ../../filesys/filesys.h
